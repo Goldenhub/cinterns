533b7bdd3e612084edd7998768c2fc7a2a821ba4
michael0221 <mosayame2002@gmail.com>
2022-07-10 10:58:05 +0100

    fetched api and added fonts

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ let closeButton = document.querySelector("button");
 
 
 let main = document.querySelector(".contain")
-
+let body = document.querySelector("html")
 fetch('https://cinterns.herokuapp.com/api/getInterns.php').then((res) => {
   // interns = res.json();
   // console.log(res.json())
@@ -15,18 +15,18 @@ fetch('https://cinterns.herokuapp.com/api/getInterns.php').then((res) => {
   console.log(data)
   internss = data;
   console.log(internss);
-  internss.forEach((element, i) => {
+  data.data.forEach((element, i) => {
     let card = document.createElement("div");
     card.classList.add("cardd");
 
     let containerContent =
       ` <div class="card">
 <div class="imgBx">
-  <img src="${internss[i].image}" alt="">
+<img src="${element.image}" alt="michael">
 </div>
 <div class="content">
   <div class="details">
-    <h2>${internss[i].name} <br> <span>${internss[i].set}</span></h2>
+    <h3 class="header">${element.name}</h3><br><span>${element.role}</span>
   </div>
 </div>
 </div>`
@@ -38,10 +38,12 @@ fetch('https://cinterns.herokuapp.com/api/getInterns.php').then((res) => {
   cards.forEach((v, i) => {
     v.addEventListener('click', () => {
       console.log("mike")
-      fillModal(internss[i]);
+      fillModal(internss.data[i])
       modal.style.display = "";
+      body.style.overflow = "hidden";
       document.querySelector(".close-btn").onclick = () => {
         modal.style.display = "none";
+        body.style.overflow = ""
       }
     })
   })
@@ -59,17 +61,15 @@ const fillModal = (int) => {
             </div>
             <div class="content__details">
                 <h3 class="content__header">${int.name}</h3>
-                <p><b>Year of Internship</b>: ${int.set}</p>
+                <p><b>Role</b>: ${int.role}</p>
+                <p><b>Year of Internship</b>: ${int.year_of_internship}</p>
                 <p><b>Email</b>: ${int.email}</p>
-                <p><b>Phone </b>: ${int.phone}</p>
+                <p><b>Address</b>: ${int.city}, ${int.state}, ${int.country}. </p>
                 <p><b>School</b>: ${int.school}</p>
+                <p><b>Major</b>: ${int.major}</p>
                 <p><b>Skills</b>:${int.skills}</p>
-                <p><b>Duration of Internship</b>: ${int.duration}</p>
       
                 <div class="socials">
-                <a href="${int.facebook}" style="color: black;" target="_blank"> <i class="ri-facebook-circle-fill" style="color: #050078;"></i></a>
-                  <a href="${int.instagram}" style="color: black;" target="_blank"> <i class="ri-instagram-line" style="color: #fa2222;"></i></a>
-                  <a href="${int.twitter}" style="color: black;" target="_blank"> <i class="ri-twitter-fill" style="color: #16B7EA;"></i></a>
                   <a href="${int.linkedin}" style="color: black;" target="_blank"> <i class="ri-linkedin-box-fill" style="color: #0077B5;"></i></a>
                   <a href="${int.github}" style="color: black;" target="_blank"><i class="ri-github-fill"></i></a>
                 </div>
@@ -77,7 +77,7 @@ const fillModal = (int) => {
         </div>
       <div class="content__footer">
         <div class="footer">
-            <p><i>"${int.testimonial}"</i></p>
+            <p><i>"${int.experience}"</i></p>
         </div>
         <div class="footer__icon">
         </div>
